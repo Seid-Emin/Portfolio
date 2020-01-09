6163fb0b04aac04fb60e7db26dc587c885a84047
Seid-Emin <seid.saydahmed@abv.bg>
2020-01-09 18:37:58 +0200

    intro updated Home page added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-    $('.intro-text').delay(500).animate({
-        opacity: 1
-    }, 1000, "linear", function () {
-        $('.intro-text1').delay(2000).fadeOut(400, 0);
-        $('.intro-text2').delay(2200).fadeOut(400, 0);
-        $('.intro-text3').delay(2400).fadeOut(400, 0);
-    });
+    $('.introSlideLeft').animate({
+        left: 0
+    }, 1500, 'easeInOutQuint');
+    $('.introSlideLeft').delay(200).animate({
+        left: "-100%"
+    }, 1500, 'easeInOutQuint');
 
-    $('.intro-divide-line').delay(4300).animate({
-        bottom: 0
-    }, 500, "linear", function () {
-        $(this).css('bottom', '-100%');
-        $('.introSlideLeft').css('left', '-100%');
-        $('.introSlideRight').css('right', '-100%');
+    $('.introSlideRight').animate({
+        right: 0
+    }, 1500, 'easeInOutQuint', function () {
+        $('.content').css('opacity', 1);
     });
-
+    $('.introSlideRight').delay(200).animate({
+        right: "-100%"
+    }, 1500, 'easeInOutQuint', function () {
+        $('.intro-window').css('display', 'none');
+    });
 });
