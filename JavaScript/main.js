a212cd97caf215697c55a14145e5f2d5aaa47e11
Seid-Emin <seid.saydahmed@abv.bg>
2020-01-10 19:42:56 +0200

    Home page finished Animation added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
+
+    //Landing Animation
+
     var introSlideLeft = $('.introSlideLeft');
     var introSlideRight = $('.introSlideRight');
     var introWindow = $('.intro-window');
     var mainContent = $('.content');
+    var body = $('.body');
+    var scrollBar = $('.scroll-bar');
 
     setTimeout(function () {
         introSlideLeft.addClass('leftShow');
@@ -19,8 +24,62 @@ $(document).ready(function () {
     }, 200);
     setTimeout(function () {
         mainContent.addClass('opacityShow');
+        body.addClass('overFlow');
+        scrollBar.addClass('opacityShow');
     }, 2000);
     setTimeout(function () {
         introWindow.addClass('displayNone');
     }, 3400);
+
+    // hover efect on projects
+    $('.projects-list .project a').hover(function () {
+        var classOfHoveredLink = $(this).attr('class').replace('project-link ', '');
+        var imgClassForShow = $('.window-wrap .window div').hasClass(classOfHoveredLink);
+        if (imgClassForShow) {
+            $('.window-wrap .window div').addClass('opacityImgOnHover');
+        }
+    }, function () {
+        $('.window-wrap .window div').removeClass('opacityImgOnHover');
+    });
+
+
+    // Nav button actvation
+    $('.menu-icon-wrap').click(function () {
+        var navClass = $('.nav').hasClass('opacityShow');
+        if (navClass) {
+            $('.nav-divide-line').removeClass('topZero');
+            $('.nav-wrap').removeClass('opacityShow');
+            setTimeout(function () {
+                $('.nav').removeClass('z-index550 opacityShow');
+                $('.menu-icon-bar').removeClass('top50');
+            }, 500);
+        } else {
+            $('.nav').addClass('z-index550 opacityShow');
+            $('.menu-icon-bar').addClass('top50');
+            setTimeout(function () {
+                $('.nav-divide-line').addClass('topZero');
+                $('.nav-wrap').addClass('opacityShow');
+            }, 500);
+        }
+    });
+
+
+    // nav projects list
+    //show
+    $('.nav-link-projects').click(function () {
+        $('.nav-projects-list').toggleClass('z-index550 opacityShow');
+        $('.nav-projects-close').toggleClass('z-index550 opacityShow');
+        $('.nav-projects-list-item').toggleClass('transformMatrix opacityShow');
+        $('.nav-list').toggleClass('opacityHide');
+    });
+
+    //hide
+    $('.nav-projects-close').click(function () {
+        $('.nav-projects-list').toggleClass('z-index550 opacityShow');
+        $('.nav-projects-close').toggleClass('z-index550 opacityShow');
+        $('.nav-projects-list-item').toggleClass('transformMatrix opacityShow');
+        setTimeout(function () {
+            $('.nav-list').toggleClass('opacityHide');
+        }, 500);
+    });
 });
