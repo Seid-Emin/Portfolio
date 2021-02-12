9188da2cab8dc857c4ed519f4e8e89a98a0078e8
seid.saydahmed <seid.saydahmed@rhythm.com>
2021-02-13 01:36:38 +0200

    ..

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -1,38 +1,32 @@
 $(document).ready(function () {
-    var introText = $('.intro-text');
-    var introText1 = $('.intro-text1');
-    var introText2 = $('.intro-text2');
-    var introText3 = $('.intro-text3');
-    var timeOuts = [700, 1000, 2200, 2400, 2600, 4600, 5300];
+    const timeOuts = [800, 1000, 2200, 2400, 2600, 4600, 5300];
 
     //    Text Show and hide
     setTimeout(function () {
-        introText.addClass('opacityShow');
+        $('.intro-text').addClass('opacityShow');
         setTimeout(function () {
-            introText1.addClass('opacityHide');
+            $('.intro-text1').addClass('opacityHide');
         }, timeOuts[2]);
         setTimeout(function () {
-            introText2.addClass('opacityHide');
+            $('.intro-text2').addClass('opacityHide');
         }, timeOuts[3]);
         setTimeout(function () {
-            introText3.addClass('opacityHide');
+            $('.intro-text3').addClass('opacityHide');
         }, timeOuts[4]);
     }, timeOuts[1]);
 
     // Curtain animation
     var introDivideLine = $('.intro-divide-line');
-    var introSlideLeft = $('.introSlideLeft');
-    var introSlideRight = $('.introSlideRight');
     setTimeout(function () {
         introDivideLine.addClass('divide-line-show');
         setTimeout(function () {
             introDivideLine.addClass('displayNone');
-        }, 800);
+        }, timeOuts[0]);
     }, timeOuts[5]);
     setTimeout(function () {
-        introSlideLeft.addClass('leftHide');
+        $('.introSlideLeft').addClass('leftHide');
     }, timeOuts[6]);
     setTimeout(function () {
-        introSlideRight.addClass('rightHide');
+        $('.introSlideRight').addClass('rightHide');
     }, timeOuts[6]);
 });
