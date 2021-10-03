9abcb6e58b20f881fb30ac2536c3a0956044dfbd
seid.saydahmed <seid.saydahmed@rhythm.com>
2021-10-04 00:00:05 +0300

    long gone

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
     //Landing Animation
-
     var introSlideLeft = $('.introSlideLeft');
     var introSlideRight = $('.introSlideRight');
     var introWindow = $('.intro-window');
@@ -67,30 +66,27 @@ $(document).ready(function () {
                 $('.nav-divide-line').addClass('topZero');
                 $('.nav-wrap').addClass('opacityShow');
             }, 500);
+
+            //nav projects list hide
+            hideMenuProjects();
         }
     });
 
 
     // nav projects list
-
     //show
     $('.nav-link-projects').click(function () {
-        $('.nav-list').toggleClass('opacityHide');
+        $('.nav-list').addClass('opacityHide');
         setTimeout(function () {
-            $('.nav-projects-list').toggleClass('z-index550 opacityShow');
-            $('.nav-projects-close').toggleClass('z-index550 opacityShow');
-            $('.nav-projects-list-item').toggleClass('transformMatrix opacityShow');
+            $('.nav-projects-list').addClass('z-index550 opacityShow');
+            $('.nav-projects-close').addClass('z-index550 opacityShow');
+            $('.nav-projects-list-item').addClass('transformMatrix opacityShow');
         }, 800);
     });
 
     //hide
-    $('.nav-projects-close').click(function () {
-        $('.nav-projects-list').toggleClass('z-index550 opacityShow');
-        $('.nav-projects-close').toggleClass('z-index550 opacityShow');
-        $('.nav-projects-list-item').toggleClass('transformMatrix opacityShow');
-        setTimeout(function () {
-            $('.nav-list').toggleClass('opacityHide');
-        }, 600);
+    $('.nav-projects-close').click(function() {
+        hideMenuProjects()
     });
 
     /* ------- PrecNext Project Hovers ------- */
@@ -110,3 +106,12 @@ $(document).ready(function () {
     });
 
 });
+
+function hideMenuProjects() {
+    $('.nav-projects-list').removeClass('z-index550 opacityShow');
+        $('.nav-projects-close').removeClass('z-index550 opacityShow');
+        $('.nav-projects-list-item').removeClass('transformMatrix opacityShow');
+        setTimeout(function () {
+            $('.nav-list').removeClass('opacityHide');
+        }, 600);
+}
